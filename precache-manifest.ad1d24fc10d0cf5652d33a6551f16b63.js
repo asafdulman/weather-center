e35caeb7e9874c19d2df1708a78d79dc7283f6a0
Asaf Dulman <asafdulman@gmail.com>
2020-11-09 14:13:11 +0200

    Updates

---
+++ precache-manifest.ad1d24fc10d0cf5652d33a6551f16b63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12e19b32eed74d9a74d664ff7ca3ada1",
+    "url": "/weather-center/index.html"
+  },
+  {
+    "revision": "53f18b3979880bbe0247",
+    "url": "/weather-center/static/css/main.85410e0e.chunk.css"
+  },
+  {
+    "revision": "6cba7e616b039b8513e2",
+    "url": "/weather-center/static/js/2.9829e713.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/weather-center/static/js/2.9829e713.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53f18b3979880bbe0247",
+    "url": "/weather-center/static/js/main.3cf2cd2f.chunk.js"
+  },
+  {
+    "revision": "38cd1b3f0b8acfeee718",
+    "url": "/weather-center/static/js/runtime-main.a2523969.js"
+  }
+]);
