fd14b8e8e8cf5cf62be4959bf638414c7d87e4b1
Asaf Dulman <asafdulman@gmail.com>
2020-11-10 10:22:11 +0200

    second commit

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -13,14 +13,26 @@ export const weatherService = {
 var favCities = []
 var citiesNames = []
 
-function getCities(value) {
-    return axios.get(`https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=mlVMZtayZdElb8tDwwm2AdErMADsCHOf&q=${value}&language=en-us`)
-        .then(res => res.data)
+async function getCities(value) {
+    const res = await axios.get(`https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=x6SNjBEgOiwSoE0Sm7AFRco0OWsPxKcT&q=${value}&language=en-us`)
+    return res.data
 }
-function getWeather(cityKey) {
-    return axios.get(`https://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityKey}?apikey=mlVMZtayZdElb8tDwwm2AdErMADsCHOf&language=en-us&details=false&metric=false`)
-        .then(res => res.data)
+// function getCities(value) {
+//     return axios.get(`https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=mlVMZtayZdElb8tDwwm2AdErMADsCHOf&q=${value}&language=en-us`)
+//         .then(res => res.data)
+// }
+async function getWeather(cityKey) {
+    const res = await axios.get(`https://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityKey}?apikey=x6SNjBEgOiwSoE0Sm7AFRco0OWsPxKcT&language=en-us&details=false&metric=false`)
+        return res.data
 }
+// function getWeather(cityKey) {
+//     return axios.get(`https://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityKey}?apikey=mlVMZtayZdElb8tDwwm2AdErMADsCHOf&language=en-us&details=false&metric=false`)
+//         .then(res => res.data)
+// }
+
+//x6SNjBEgOiwSoE0Sm7AFRco0OWsPxKcT
+//jbQCGr5hw3CneCAiXlvKodc3ASLREpxy
+//17nM18URNW0l13ncG90uLIxINREYuwzk
 
 
 function saveCity(city) {
